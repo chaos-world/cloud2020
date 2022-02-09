908d7b8b474022204cc9d1891eb7a4bd50b45597
chaos-world <598758101@qq.com>
2022-02-09 13:52:02 +0800

    First Modification

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import feign.Logger;
+
+/**
+ * @auther zzyy
+ * @create 2019-11-10 17:00
+ */
+@Configuration
+public class FeignConfig
+{
+    @Bean
+    Logger.Level feignLoggerLevel()
+    {
+        return Logger.Level.FULL;
+    }
+}
